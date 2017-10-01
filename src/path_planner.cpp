53ad673f1543662bdda5717ef5d30281b32e83d9
ricgu8086 <ricgu8086@gmail.com>
2017-10-01 03:15:08 -0700

    Just a big refactor. All code exclusively related to path planning (and auxiliary functions) was moved to another file to increase clarity and development easiness.

---
+++ src/path_planner.cpp
@@ -0,0 +1,197 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+/*** Previously in main.cpp ***/
+
+// For converting back and forth between radians and degrees.
+constexpr double pi()
+{
+	return M_PI;
+}
+double deg2rad(double x)
+{
+	return x * pi() / 180;
+}
+double rad2deg(double x)
+{
+	return x * 180 / pi();
+}
+
+double distance(double x1, double y1, double x2, double y2)
+{
+	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+int ClosestWaypoint(double x, double y, vector<double> maps_x,
+		vector<double> maps_y)
+{
+
+	double closestLen = 100000; //large number
+	int closestWaypoint = 0;
+
+	for (int i = 0; i < maps_x.size(); i++)
+	{
+		double map_x = maps_x[i];
+		double map_y = maps_y[i];
+		double dist = distance(x, y, map_x, map_y);
+		if (dist < closestLen)
+		{
+			closestLen = dist;
+			closestWaypoint = i;
+		}
+
+	}
+
+	return closestWaypoint;
+
+}
+
+int NextWaypoint(double x, double y, double theta, vector<double> maps_x,
+		vector<double> maps_y)
+{
+
+	int closestWaypoint = ClosestWaypoint(x, y, maps_x, maps_y);
+
+	double map_x = maps_x[closestWaypoint];
+	double map_y = maps_y[closestWaypoint];
+
+	double heading = atan2((map_y - y), (map_x - x));
+
+	double angle = abs(theta - heading);
+
+	if (angle > pi() / 4)
+	{
+		closestWaypoint++;
+	}
+
+	return closestWaypoint;
+
+}
+
+// Transform from Cartesian x,y coordinates to Frenet s,d coordinates
+vector<double> getFrenet(double x, double y, double theta,
+		vector<double> maps_x, vector<double> maps_y)
+{
+	int next_wp = NextWaypoint(x, y, theta, maps_x, maps_y);
+
+	int prev_wp;
+	prev_wp = next_wp - 1;
+	if (next_wp == 0)
+	{
+		prev_wp = maps_x.size() - 1;
+	}
+
+	double n_x = maps_x[next_wp] - maps_x[prev_wp];
+	double n_y = maps_y[next_wp] - maps_y[prev_wp];
+	double x_x = x - maps_x[prev_wp];
+	double x_y = y - maps_y[prev_wp];
+
+	// find the projection of x onto n
+	double proj_norm = (x_x * n_x + x_y * n_y) / (n_x * n_x + n_y * n_y);
+	double proj_x = proj_norm * n_x;
+	double proj_y = proj_norm * n_y;
+
+	double frenet_d = distance(x_x, x_y, proj_x, proj_y);
+
+	//see if d value is positive or negative by comparing it to a center point
+
+	double center_x = 1000 - maps_x[prev_wp];
+	double center_y = 2000 - maps_y[prev_wp];
+	double centerToPos = distance(center_x, center_y, x_x, x_y);
+	double centerToRef = distance(center_x, center_y, proj_x, proj_y);
+
+	if (centerToPos <= centerToRef)
+	{
+		frenet_d *= -1;
+	}
+
+	// calculate s value
+	double frenet_s = 0;
+	for (int i = 0; i < prev_wp; i++)
+	{
+		frenet_s += distance(maps_x[i], maps_y[i], maps_x[i + 1],
+				maps_y[i + 1]);
+	}
+
+	frenet_s += distance(0, 0, proj_x, proj_y);
+
+	return
+	{	frenet_s,frenet_d};
+
+}
+
+// Transform from Frenet s,d coordinates to Cartesian x,y
+vector<double> getXY(double s, double d, vector<double> maps_s,
+		vector<double> maps_x, vector<double> maps_y)
+{
+	int prev_wp = -1;
+
+	while (s > maps_s[prev_wp + 1] && (prev_wp < (int) (maps_s.size() - 1)))
+	{
+		prev_wp++;
+	}
+
+	int wp2 = (prev_wp + 1) % maps_x.size();
+
+	double heading = atan2((maps_y[wp2] - maps_y[prev_wp]),
+			(maps_x[wp2] - maps_x[prev_wp]));
+	// the x,y,s along the segment
+	double seg_s = (s - maps_s[prev_wp]);
+
+	double seg_x = maps_x[prev_wp] + seg_s * cos(heading);
+	double seg_y = maps_y[prev_wp] + seg_s * sin(heading);
+
+	double perp_heading = heading - pi() / 2;
+
+	double x = seg_x + d * cos(perp_heading);
+	double y = seg_y + d * sin(perp_heading);
+
+	return
+	{	x,y};
+
+}
+
+/*** /Previously in main.cpp ***/
+
+
+
+
+//1 mph is equal to 0.44704 meter/second.
+inline double mph2mps(double mph)
+{
+	return mph * 0.44704;
+}
+
+inline double mps2mph(double mps)
+{
+	return mps / 0.44704;
+}
+
+void path_planner(vector<double> &next_x_vals, vector<double> &next_y_vals, \
+					double car_s, vector<double> &map_waypoints_s, \
+					vector<double> &map_waypoints_x, vector<double> &map_waypoints_y)
+{
+	double dist_inc = 0.5;
+	double next_s, next_d;
+	vector<double> xy;
+
+	for(int i = 0; i < 50; i++)
+	{
+		next_s = car_s + (i+1)*dist_inc;
+		next_d = 6;
+
+		xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+		next_x_vals.push_back(xy[0]);
+		next_y_vals.push_back(xy[1]);
+	}
+
+	// TODO DEBUG
+	/*for(int i = 0; i < 50; i++)
+	{
+		printf("(%lf, %lf)", next_x_vals[i], next_y_vals[i]);
+	}*/
+}
---
+++ src/path_planner.h
@@ -0,0 +1,3 @@
+void path_planner(std::vector<double> &next_x_vals, std::vector<double> &next_y_vals, \
+					double car_s, std::vector<double> &map_waypoints_s, \
+					std::vector<double> &map_waypoints_x, std::vector<double> &map_waypoints_y);
